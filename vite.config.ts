571ae433edac55be8b84d250ecdbb610ea1b5604
Winkassador! <winkassador@gmail.com>
2025-09-13 03:54:59 +0100

    Change base path from petitionle to petitionsplease

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/petitionle/",
+  base: "/petitionsplease/",
   server: {
     fs: {
       strict: false
